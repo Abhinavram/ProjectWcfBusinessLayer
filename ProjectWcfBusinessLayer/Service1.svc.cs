f83bc994c9703eab7c7c966b1b6329385344dc96
AbhinavRam <avedantamkodanda@unomaha.edu>
2017-07-31 16:31:02 -0500

    Initial commit with entity framework model database, employyes and customer

--- ProjectWcfBusinessLayer/Service1.svc.cs
+++ ProjectWcfBusinessLayer/Service1.svc.cs
@@ -12,22 +12,11 @@ namespace ProjectWcfBusinessLayer
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
+        ProjectDBEntities db = new ProjectDBEntities();
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public List<Employee> GetAllEmployees()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return db.Employees.ToList();
         }
     }
 }
